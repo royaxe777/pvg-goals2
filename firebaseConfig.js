3514a1df46128a664121982e787147dab3502694
royaxe777 <joomlastic@gmail.com>
2025-08-27 15:34:09 +0800

    Firebase Configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyChKJ-fo7JDv7-7B6FhuKHwmuU_Z6IKvsE",
+  authDomain: "pvg-goal.firebaseapp.com",
+  projectId: "pvg-goal",
+  storageBucket: "pvg-goal.firebasestorage.app",
+  messagingSenderId: "135060764408",
+  appId: "1:135060764408:web:a572e3b584762d94d76d0d"
+};
+
+const app = initializeApp(firebaseConfig);
